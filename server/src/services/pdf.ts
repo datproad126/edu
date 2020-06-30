9ba7644ca7cfedbff4c095540982b85c3d7acb20
asia kite <asiakite@asias-Mac-mini.local>
2020-06-30 11:53:41 +0700

    update pdf.ts

--- server/src/services/pdf.ts
+++ server/src/services/pdf.ts
@@ -1,15 +1,15 @@
 import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib'
 import { Request, Response } from 'express'
-import fetch from 'node-fetch'
+import fetch, { Blob } from 'node-fetch'
+import { saveAs } from 'file-saver';
 const overridePdf = async (req: Request, res: Response): Promise<any> => {
   try {
-    console.log(req.body);
-    if (req.body.session)
+    if (req.body.session === "")
     return res.status(400).send({
       success: 'false',
       message: 'session is required'
     })
-  else if (req.body.url)
+  else if (req.body.url === "")
     return res.status(400).send({
       success: 'false',
       message: 'url is required'
@@ -27,16 +27,21 @@ const overridePdf = async (req: Request, res: Response): Promise<any> => {
       y: 100,
       size: 18,
       font: helveticaFont,
-      color: rgb(0, 0.78, 1)
+      color: rgb(1, 1, 1)
     })
   });
 
-  const pdfBytes = await pdfDoc.save()
-  console.log(pdfBytes);
-  return res.status(201).send({
-    success: 'true',
-    message: 'handling pdf is successfuly'
-  })
+  const pdfBytes = await pdfDoc.saveAsBase64();
+  const getEncodePDF = (encodedPDF: string) => {
+    res.writeHead(200,  {
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'attachment; filename="example.pdf"'
+    });
+
+    const download = Buffer.from(encodedPDF, 'base64');
+    res.end(download);
+  };
+  return getEncodePDF(pdfBytes);
   } catch (error) {
     console.error(error);
   }
