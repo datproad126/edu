10d970ed6899a06f2eb1a37c09a0170c963d9027
asia kite <asiakite@asias-Mac-mini.local>
2020-07-08 12:02:03 +0700

    get text into pdf

--- server/src/controllers/pdf.ts
+++ server/src/controllers/pdf.ts
@@ -4,31 +4,43 @@ import * as express from 'express'
 import IControllerBase from '../interfaces/IControllerBase.interface'
 import fetch from 'node-fetch'
 import multer from 'multer'
+import fs from 'fs'
+import path, { join } from 'path'
+import { type } from 'os'
+const pdfToText = require('pdf-to-text')
 
-const storage = multer.diskStorage({
-  destination: (req: Request, _file: any, cb: any) => {
-    cb(null, '/storage/uploads/');
-    // const dir = './storage/';
-    // mkdirp(dir, (err: NodeJS.ErrnoException) => {
-    //     cb(err, dir)
-    // });
-  },
-  filename: (_req: Request, file: any, cb: any) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
-  }
-})
 
 class PdfController implements IControllerBase {
   public path = '/api/v1/pdf/'
   public router = express.Router()
-  private upload = multer({ dest: 'storage/uploads/' })
+  private upload: any
+  private storage: any
+  private pages: any
 
   constructor() {
     this.initRoutes()
   }
 
   public initRoutes() {
+    this.storage = multer.diskStorage({
+      destination: (req: Request, _file: any, cb: any) => {
+        try {
+          cb(null, path.join(__dirname, '../../storage/uploads/'));
+        } catch (error) {
+          console.log(error);
+        }
+      },
+      filename: (_req: Request, file: any, cb: any) => {
+        const uniqueSuffix = new Date().toISOString().replace(/[\/\\:]/g, "_") + file.originalname
+        cb(null, uniqueSuffix)
+      }
+    })
+    this.upload = multer({
+      storage: this.storage,
+      limits: { fileSize: 1000000 }
+      // dest: '/storage/uploads/'
+    })
+
     this.router.post(this.path + 'override-pdf', this.overridePdf)
     this.router.post(this.path + 'get-text', this.upload.single('pdf'), this.getText)
   }
@@ -38,12 +50,12 @@ class PdfController implements IControllerBase {
       console.log(req.body);
       if (req.body.session === "")
         return res.status(400).send({
-          success: 'false',
+          status: 'fail',
           message: 'session is required'
         })
       else if (req.body.url === "")
         return res.status(400).send({
-          success: 'false',
+          status: 'fail',
           message: 'url is required'
         })
       // handling pdf
@@ -52,17 +64,24 @@ class PdfController implements IControllerBase {
       const pdfDoc = await PDFDocument.load(existingPdfBytes)
       const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
 
-      const pages = pdfDoc.getPages()
-      pages.forEach(element => {
-        element.drawText(req.body.session, {
-          x: 5,
-          y: 100,
-          size: 18,
-          font: helveticaFont,
-          color: rgb(1, 1, 1)
-        })
+      this.pages = pdfDoc.getPages()
+      // this.pages.forEach((element: any) => {
+      //   element.drawText(req.body.session, {
+      //     x: 5,
+      //     y: 100,
+      //     size: 18,
+      //     font: helveticaFont,
+      //     color: rgb(1, 1, 1)
+      //   })
+      // });
+      const randomPage = this.pages[45]
+      randomPage.drawText(req.body.session, {
+        x: 5,
+        y: 100,
+        size: 18,
+        font: helveticaFont,
+        color: rgb(0, 1, 1)
       });
-
       const pdfBytes = await pdfDoc.saveAsBase64();
       const getEncodePDF = (encodedPDF: string) => {
         res.writeHead(200, {
@@ -79,10 +98,38 @@ class PdfController implements IControllerBase {
     }
   }
 
-  public getText = (req: Request, res: Response, next: any): any => {
+  public getText = async (req: Request, res: Response, next: any): Promise<any> => {
     try {
-      console.log(req.file);
-      console.log(req.body);
+      // console.log(req.file);
+      // console.log(req.body.session);
+      if (!req.file) {
+        res.status(400).send({
+          status: 'fail',
+          message: 'file is not found'
+        })
+      }
+      else if (!req.body.session) {
+        res.status(400).send({
+          status: 'fail',
+          message: 'session is not found'
+        })
+      }
+      const foundsession = {}
+      // install poppler-until
+      pdfToText.info(req.file.path, (err: any, info: any) => {
+        if (err) throw (err);
+        for (let index = 1; index <= info.pages; index++) {
+          const option = { from: index, to: index }
+
+          pdfToText.pdfToText(req.file.path, option, (err: any, data: any) => {
+            if (err) throw (err);
+            if (data.includes(req.body.session)) {
+              console.log(index)
+            }
+          });
+        }
+      });
+
     } catch (error) {
       console.error(error);
     }
